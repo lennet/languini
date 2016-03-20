962fcc80823796020c3cba4dcc7af84aa8e28682
Daniel Steinmetz <daniel.steinmetz@posteo.de>
2016-03-20 17:57:47 +0100

    init new language overview

---
+++ app/LanguageQuiz/LanguagesOverview/SelectionMarker.swift
@@ -0,0 +1,21 @@
+//
+//  SelectionMarker.swift
+//  Languini
+//
+//  Created by Daniel Steinmetz on 20.03.16.
+//  Copyright © 2016 Coding Da Vinci. All rights reserved.
+//
+
+import UIKit
+
+class SelectionMarker: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
