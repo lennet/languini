0e3ed20902732c5c793bb27f9398a28235056fce
Leonard Thomas <leo.thomas@me.com>
2016-04-17 12:22:00 +0200

    introduced infoviewcontroller

---
+++ app/LanguageQuiz/Menu/InfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoViewController.swift
+//  Languini
+//
+//  Created by Leo Thomas on 17/04/16.
+//  Copyright © 2016 Coding Da Vinci. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    @IBAction func dismiss() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
